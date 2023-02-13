0ba1350d03ac7edcee10690e948c2001067dc20c
shakerAbuDrais <thecoffeiner@gmail.com>
2023-02-13 10:42:36 +0300

    Fix minor issues

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,6 +17,6 @@ class UsersController < ApplicationController
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:full_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:full_name, :password, :password_confirmation ])
   end
 end
