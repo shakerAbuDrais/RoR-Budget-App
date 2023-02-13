1fd0aa2eace55c08116b2abc955c4f8251ad02d7
shakerAbuDrais <thecoffeiner@gmail.com>
2023-02-13 06:03:53 +0300

    Finish the styling of login and signup, create splash page with the name of the app

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,19 @@
 class UsersController < ApplicationController
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to root_path, notice: "Sign up successful."
+    else
+      render :new
+    end
+  end  
+
+  def index
+    @users = User.all
+  end  
+
   protected
 
   def configure_permitted_parameters
