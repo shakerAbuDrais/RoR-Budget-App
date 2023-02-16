f3c284679838f1f8721ff21b9accab4595dde7db
shakerAbuDrais <thecoffeiner@gmail.com>
2023-02-16 20:04:58 +0300

    Add hosting reqs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -33,10 +33,10 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
-
+  config.serve_static_files = true
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
-
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for Apache
   # config.action_dispatch.x_sendfile_header = "X-Accel-Redirect" # for NGINX
