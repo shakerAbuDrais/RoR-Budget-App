4fe474061b7794834c1604718d933fdf4720012d
shakerAbuDrais <thecoffeiner@gmail.com>
2023-02-16 20:16:45 +0300

    Fix minor mistake

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,6 +1,6 @@
 // Import and register all your controllers from the importmap under controllers/*
 
-import { Application } from "./application";
+import { application } from "./application";
 // Lazy load controllers as they appear in the DOM (remember not to preload controllers in import map!)
 // import { lazyLoadControllersFrom } from "@hotwired/stimulus-loading"
 // lazyLoadControllersFrom("controllers", application)
